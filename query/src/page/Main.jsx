6f2005c12aaff3b58b27d10236ce79ac36261c8a
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-02 10:55:42 +0200

    loading added

--- query/src/page/Main.jsx
+++ query/src/page/Main.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPersons } from '../queries/getPersons';
 import { request } from "graphql-request";
+import PuffLoader from "react-spinners/PuffLoader";
+
 
 
 export const Main = () => {
@@ -14,16 +16,27 @@ export const Main = () => {
 
   console.log("Data", data);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <PuffLoader size= {30} color = "white"/>;
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
   
   
-    return (
-      <>
-        <h1>Hello world</h1>
-      </>
-    );
-  };
+  return (
+    <section>
+      <h1>Hello Mainpage</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 1fr)",
+          textAlign: "left",
+        }}
+      >
+        {data.allPeople.people.map((item, i) => {
+          return <p key={i}>{item.name}</p>;
+        })}
+      </div>
+    </section>
+  );
+};
