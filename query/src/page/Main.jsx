6fcb91df6368bb00f23bd75f2a9db5fbdaa38376
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-02 10:19:17 +0200

    works-gets data out in consollog

--- query/src/page/Main.jsx
+++ query/src/page/Main.jsx
@@ -1,9 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPersons } from '../queries/getPersons';
+import { request } from "graphql-request";
+
+
 export const Main = () => {
+    const { data, isLoading, error } = useQuery({
+      queryKey: ["getStarwarsPerson"],
+      queryFn: async () => request(
+        `https://swapi-graphql.netlify.app/.netlify/functions/index`, 
+        getPersons
+      ),
+    });
 
+  console.log("Data", data);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+  
+  
     return (
-        <>
-    <h1>Hello World, this is the mainpage</h1>
-        </>
-      )
-
-}
+      <>
+        <h1>Hello world</h1>
+      </>
+    );
+  };
